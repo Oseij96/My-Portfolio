b03c9c017b884673ae792a83b8822aa1611097cc
Joel Osei <oseij96@gmail.com>
2023-07-29 16:08:55 +0100

    Update to build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-
-export default defineConfig({
-  plugins: [react()]
-})
+export default {
+  build: {
+    rollupOptions: {
+      external: ['/assets/index-56f02ced.js'],
+    },
+  },
+};
