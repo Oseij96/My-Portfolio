ffab284d701f19c699db12ee32d7d8135a36dd32
Joel Osei <oseij96@gmail.com>
2023-07-30 09:55:50 +0100

    Minor Update to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,12 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="py-5 text-center">
-            {/* {Social Icons} */}
+            <a
+                href="https://github.com/Oseij96"
+                className="text-sm text-gray-500 hover:text-gray-800 transition duration-300"
+            >
+                <i className="fab fa-github-square text-2xl"></i>
+            </a>
             <p className="text-sm mt-2 opacity-50">
                 &copy; {new Date().getFullYear()} Joel Osei. All rights reserved.
             </p>
